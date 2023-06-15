dfeecad2ac319f9b987f873d59588b77c4bc7168
FIRST_NAME LAST_NAME <20520722@gm.uit.edu.vn>
2023-06-15 22:11:31 +0700

    commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/')
+@app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    app.run(debug=True)
